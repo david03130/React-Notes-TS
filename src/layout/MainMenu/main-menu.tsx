b118a25fe9b3cfa05eb76143a2b8562100a8be4e
David Salcedo <david.salcedo0313@gmail.com>
2023-06-30 17:25:41 +0200

    Main menu refactor

--- src/layout/MainMenu/main-menu.tsx
+++ src/layout/MainMenu/main-menu.tsx
@@ -1,17 +1,18 @@
 import "./main-menu.css";
 
 import { SmallButton } from "../../components/common/SmallButton";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 
 const MainMenu = (): JSX.Element => {
   const buttonHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     console.log("Click!");
-  }
+  };
 
   return (
     <div className="main-menu">
       <h1>Notes</h1>
-      <SmallButton clickEvent={buttonHandler}/>
+      <SmallButton icon={faAdd} clickEvent={buttonHandler} />
     </div>
   );
 };
