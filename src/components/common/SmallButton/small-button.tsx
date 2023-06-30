10498d95644727d2415364ef5f782e34580ffb94
David Salcedo <david.salcedo0313@gmail.com>
2023-06-30 17:25:26 +0200

    Small button uses FA Icons

--- src/components/common/SmallButton/small-button.tsx
+++ src/components/common/SmallButton/small-button.tsx
@@ -1,14 +1,19 @@
 import "./small-button.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
 interface SmallButtonProps {
-  icon?: string;
+  icon: IconDefinition;
   clickEvent: React.MouseEventHandler;
 }
 
-const SmallButton = ({icon, clickEvent}: SmallButtonProps): JSX.Element => {
-  return <a onClick={clickEvent} href="">
-    +<i></i>
-  </a>;
+const SmallButton = ({ icon, clickEvent }: SmallButtonProps): JSX.Element => {
+  return (
+    <a onClick={clickEvent} href="">
+      <FontAwesomeIcon icon={icon} />
+    </a>
+  );
 };
 
 export default SmallButton;
