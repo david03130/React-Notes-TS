5d4440db84bae09b61263bdf24bfc7c325729ea0
David Salcedo <david.salcedo0313@gmail.com>
2023-06-30 13:43:42 +0200

    Home updated

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -2,11 +2,12 @@ import "./home.css";
 
 import React from "react";
 import { Notes } from "../../components/Notes";
+import { MainMenu } from "../../layout/MainMenu";
 
 const Home = (): JSX.Element => {
   return (
     <div className="home">
-      <h1>Notes</h1>
+      <MainMenu />
       <Notes />
     </div>
   );
