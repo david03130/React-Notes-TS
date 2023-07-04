9f3b9432ff4e015c77d185ad00ccef43be784675
David Salcedo <david.salcedo0313@gmail.com>
2023-07-04 17:45:41 +0200

    New service file NoteCalls added

---
+++ src/services/note-calls.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { Note } from "../components/note";
+
+const baseUrl = "/api/notes";
+
+const getAll = (): Promise<Note[]> => {
+  const request = axios.get<Note[]>(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (note: Note) => {
+  return axios.post<Note>(baseUrl, note);
+};
+
+export default { getAll, create };
