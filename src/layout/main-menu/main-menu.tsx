dde09acca77d94dcfa5258ef6981f270738cca7a
David Salcedo <david.salcedo0313@gmail.com>
2023-07-07 11:01:29 +0200

    MainMenu component changed to work with Modal

--- src/layout/main-menu/main-menu.tsx
+++ src/layout/main-menu/main-menu.tsx
@@ -2,17 +2,28 @@ import "./main-menu.css";
 
 import { SmallButton } from "../../components/common/small-button";
 import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { Modal } from "../../components/common/modal";
 
 const MainMenu = (): JSX.Element => {
+  const [visibleModal, setVisibleModal] = useState(false);
+  const showModal = () => setVisibleModal(true);
+  const hideModal = () => setVisibleModal(false);
+
   const buttonHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    console.log("Click!");
+    showModal();
   };
 
   return (
     <div className="main-menu">
       <h1>Notes</h1>
       <SmallButton icon={faAdd} clickEvent={buttonHandler} />
+      <Modal
+        children={<p>Hello world!</p>}
+        isOpen={visibleModal}
+        handleClose={hideModal}
+      />
     </div>
   );
 };
