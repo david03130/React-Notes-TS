ed1f1f0a01d0940c0f99478d148b993e35d740ba
David Salcedo <david.salcedo0313@gmail.com>
2023-07-12 13:15:41 +0200

    Change NoteDetails pass important icon to Modal

--- src/components/note-details/note-details.tsx
+++ src/components/note-details/note-details.tsx
@@ -1,3 +1,7 @@
+import "./note-details.css";
+
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import { Modal } from "../common/modal";
 import Note from "../note/note";
 
@@ -12,11 +16,22 @@ const NoteDetails = ({
   modalVisibility,
   handleClose,
 }: NoteDetailsProps) => {
+  let extraIconsArray: FontAwesomeIconProps[] = [];
+
+  if (note.important) {
+    const importantIcon: FontAwesomeIconProps = {
+      icon: faExclamation,
+      className: "modal__important-note",
+    };
+    extraIconsArray.push(importantIcon);
+  }
+
   return (
     <Modal
       modalTitle={note.title}
       isOpen={modalVisibility}
       handleClose={handleClose}
+      extraIcons={extraIconsArray}
     >
       <p>{note.content}</p>
     </Modal>
