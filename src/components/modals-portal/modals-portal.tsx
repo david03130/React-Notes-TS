0487830c85e9ee957e19331bc660b04b4982124d
David Salcedo <david.salcedo0313@gmail.com>
2023-07-21 17:35:48 +0200

    Change ModalsPortal opens details now

--- src/components/modals-portal/modals-portal.tsx
+++ src/components/modals-portal/modals-portal.tsx
@@ -1,10 +1,13 @@
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { hideNoteDetails } from "../../store/reducers/note-details-slice";
 import { hideNoteForm } from "../../store/reducers/note-form-slice";
 import { ReactPortal } from "../common/react-portal";
+import { NoteDetails } from "../note-details";
 import { NoteForm } from "../note-form";
 
 const ModalsPortal = () => {
   const noteForm = useAppSelector((state) => state.noteFormModal);
+  const noteDetails = useAppSelector((state) => state.noteDetailsModal);
   const dispatch = useAppDispatch();
   return (
     <div>
@@ -16,6 +19,14 @@ const ModalsPortal = () => {
             dispatch(hideNoteForm());
           }}
           noteData={noteForm.noteData}
+        />
+        <NoteDetails
+          modalVisibility={noteDetails.visibility}
+          handleClose={(e) => {
+            e.preventDefault();
+            dispatch(hideNoteDetails());
+          }}
+          note={noteDetails.noteData}
         />
       </ReactPortal>
     </div>
