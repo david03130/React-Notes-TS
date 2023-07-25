0b68f020f2a31b9557979af1bbfc815c773536a3
David Salcedo <david.salcedo0313@gmail.com>
2023-07-25 13:01:40 +0200

    Change NotesDropdown has dispatch for notesToShow

--- src/components/notes-dropdown/notes-dropdown.tsx
+++ src/components/notes-dropdown/notes-dropdown.tsx
@@ -1,10 +1,17 @@
 import "./notes-dropdown.css";
 import Dropdown, { DropdownOption } from "../common/dropdown";
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { NotesToShow, setNotesToShow } from "../../store/reducers/notes-slice";
 
 const NotesDropdown = () => {
+  const dispatch = useAppDispatch();
+
+  const changeEvent = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setNotesToShow(e.target.value as NotesToShow));
+  };
   const dropdownOptions: DropdownOption[] = [
-    { value: "not-important", title: "Not important" },
-    { value: "important", title: "Important" },
+    { value: "all", title: "All" },
+    { value: "important", title: "Only Important" },
   ];
   return (
     <Dropdown
@@ -12,6 +19,7 @@ const NotesDropdown = () => {
       dropdownName="notes_importance"
       dropdownId="notes_importance"
       options={dropdownOptions}
+      changeEvent={changeEvent}
     />
   );
 };
