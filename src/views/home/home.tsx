6c76e29980e5da98369672ac3cf59f2c2fc3d6d0
David Salcedo <david.salcedo0313@gmail.com>
2023-07-25 12:30:52 +0200

    Change NotesDropdwon added to Home

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -3,11 +3,13 @@ import "./home.css";
 import { Notes } from "../../components/notes";
 import { MainMenu } from "../../layout/main-menu";
 import { ModalsPortal } from "../../components/modals-portal";
+import NotesDropdown from "../../components/notes-dropdown";
 
 const Home = (): JSX.Element => {
   return (
     <div className="home">
       <MainMenu />
+      <NotesDropdown />
       <Notes />
       <ModalsPortal />
     </div>
